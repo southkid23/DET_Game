c4003af5dc033ea5009239f4be7bb30006462fff
ShawnYcx <scy12a@acu.edu>
2014-11-16 16:14:21 -0600

    Game.lua updated

--- DET/game.lua
+++ DET/game.lua
@@ -1,6 +1,8 @@
 
 local composer = require( "composer" )
 local scene = composer.newScene()
+local speed = 1000
+local count = 10
 
 
 function scene:create(event)
@@ -9,10 +11,11 @@ function scene:create(event)
 	local background = display.newImage("clouds.jpg")
 	background.x = 0
 	background.y = 240
-	textScore = display.newText("Score: "..score, 25, -25, nil, 12)
+	textScore = display.newText("Score: "..score, 50, -0, nil, 12)
    	textScore:setTextColor(1,1,1)
 
    	local rect = display.newRect(160, 530, display.contentWidth, 10)
+   	rect.myName = "obj"
    	physics.addBody(rect, "static", {})
 
 end
@@ -38,16 +41,25 @@ end
 local function drops()
 
 	local rings = display.newImageRect("Rings.png", 30, 30)
+	rings.myName = "ring"
 	rings.x, rings.y = 160, -120
 	rings.rotation = 20
 
-	rings.x = 1 + math.random( 300 ); rings.y = -20
+	rings.x = math.random(15, 300); rings.y = -70
 
 	physics.addBody( rings, { density=1.0, friction=0.3, bounce=0.3 } )
 
 	rings.touch = onRingTouch
 	rings:addEventListener("touch", rings)
 
+end
+
+-- Sleeps for x milliseconds
+function delayRings()
+
+	count=count*1.4
+	speed=speed/1.4
+	timer.performWithDelay(speed, drops, count)
 end
 
 function onRingTouch(self, event)
@@ -61,9 +73,21 @@ function onRingTouch(self, event)
 	return true
 end
 
+function onCollision(event)
+	if((event.object1.myName=="ring" and event.object2.myName=="obj") or
+	(event.object1.myName=="obj" and event.object2.myName=="ring")) then
+		timer.performWithDelay( 100, function() physics.stop() end, 1 )
+		local single = display.newImage("forever-alone.jpg")
+		single:scale(.5, .5)
+		single.x = 160
+		single.y = 260
+	end
+end
 
 scene:addEventListener( "create", scene )
+Runtime:addEventListener("collision", onCollision)
 newText()
 timer.performWithDelay(1000, drops, 10)
+timer.performWithDelay(10000, delayRings, 2)
 
 return scene
